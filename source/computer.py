ef40d8c4d6c741d6e632df60ef922d7f8f63fd60
arata <a.kuse.sys24@morijyobi.ac.jp>
2024-11-14 11:29:18 +0900

    player.py作成_#1

--- source/computer.py
+++ source/computer.py
@@ -1,1 +1,4 @@
+import random
 
+def computer_pon():
+    return random.randint(1, 3)
--- source/player.py
+++ source/player.py
@@ -1,1 +1,7 @@
-
+def human_pon():
+    while True:
+        janken = int(input("1.グー\n2.チョキ\n3.パー\nあなたの手を選択してください。> "))
+        if janken in [1, 2, 3]:
+            return janken
+        else:
+            print("正しい数値で入力してください。")
